5e8f91e03494cff4ab5ab39dc2cf71c1514f1216
jaygil8755 <68841210+jaygil8755@users.noreply.github.com>
2023-05-16 23:51:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -516,7 +516,8 @@ if choose == "Bertopic":
 #         st.write('초록이 있는 논문 수', len(text))
     
         # 토크나이저에 명사만 추가한다
-#         st.write('명사를 추출합니다..')
+        st.write('명사를 추출합니다..')
+
         extract_pos_list = ["NNG", "NNP", "NNB", "NR", "NP"]
         stopwords = Stopwords()
         
